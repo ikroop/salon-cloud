a0da202ec882f60823d6d442ea28eb64e6874d5f
Thanh Truong <vagabondtt1503@gmail.com>
2016-08-11 14:06:24 -0400

    fix bug: database URL

--- SalonCloud.Server/Config/dev/database.js
+++ SalonCloud.Server/Config/dev/database.js
@@ -1,5 +1,5 @@
-// config/database.js
+// config/dev/database.js
 module.exports = {
-    'url': '  mongodb://saloncloud:siamtian@2015@ds145365.mlab.com:45365/salon-cloud' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://saloncloud:siamtian@2015@ds145365.mlab.com:45365/salon-cloud'
 };
 //# sourceMappingURL=database.js.map
--- SalonCloud.Server/Config/dev/database.ts
+++ SalonCloud.Server/Config/dev/database.ts
@@ -1,4 +1,4 @@
-﻿// config/database.js
+﻿// config/dev/database.js
 module.exports = {
-    'url': '  mongodb://saloncloud:siamtian@2015@ds145365.mlab.com:45365/salon-cloud' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://saloncloud:siamtian@2015@ds145365.mlab.com:45365/salon-cloud'
 };
