fcb057d8896eb005b0dd0e0e2ea820e8d088b1a6
Thanh Truong <vagabondtt1503@gmail.com>
2016-08-12 22:58:05 -0400

    add login test case

--- SalonCloud.Server/spec/Authentication.spec.js
+++ SalonCloud.Server/spec/Authentication.spec.js
@@ -14,7 +14,7 @@ describe('Routing', function () {
         mongoose.connect(configDB.url);
         done();
     });
-    // use describe to give a title to your test suite, in this case the tile is "Account"
+    // use describe to give a title to your test suite, in this case the tile is "User"
     // and then specify a function in which we are going to declare all the tests
     // we want to run. Each test starts with the function it() and as a first argument 
     // we have to provide a meaningful title for it, whereas as the second argument we
@@ -22,6 +22,8 @@ describe('Routing', function () {
     // to specify when our test is completed, and that's what makes easy
     // to perform async test!
     describe('User Register', function () {
+        var apiUrl = /auth/register;
+
         it('should return "MissingUsername" error trying to register without username', function (done) {
             var user = {
                 password: '123456'
@@ -31,7 +33,7 @@ describe('Routing', function () {
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
             request(url)
-                .post('/auth/register')
+                .post(apiUrl)
                 .send(user)
                 // end handles the response
                 .end(function (err, res) {
@@ -56,7 +58,7 @@ describe('Routing', function () {
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
             request(url)
-                .post('/auth/register')
+                .post(apiUrl)
                 .send(user)
                 // end handles the response
                 .end(function (err, res) {
@@ -81,7 +83,7 @@ describe('Routing', function () {
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
             request(url)
-                .post('/auth/register')
+                .post(apiUrl)
                 .send(user)
                 // end handles the response
                 .end(function (err, res) {
@@ -105,7 +107,7 @@ describe('Routing', function () {
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
             request(url)
-                .post('/auth/register')
+                .post(apiUrl)
                 .send(user)
                 // end handles the response
                 .end(function (err, res) {
@@ -130,7 +132,7 @@ describe('Routing', function () {
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
             request(url)
-                .post('/auth/register')
+                .post(apiUrl)
                 .send(user)
                 // end handles the response
                 .end(function (err, res) {
@@ -155,7 +157,7 @@ describe('Routing', function () {
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
             request(url)
-                .post('/auth/register')
+                .post(apiUrl)
                 .send(user)
                 // end handles the response
                 .end(function (err, res) {
@@ -181,7 +183,7 @@ describe('Routing', function () {
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
             request(url)
-                .post('/auth/register')
+                .post(apiUrl)
                 .send(user)
                 // end handles the response
                 .end(function (err, res) {
@@ -194,8 +196,134 @@ describe('Routing', function () {
                     res.body.err.should.have.property('username').eql(user.username);
                     done();
                 });
+        });        
+
+        it('should return "UsernameAlreadyExists" trying to register with existing username', function (done) {
+            var user = {
+                username: 'unittest@gmail.com',
+                password: '123456',
+                fullname: 'salonhelps'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(user)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(409);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('UsernameAlreadyExists');
+                    done();
+                });
+        });    
+    });
+
+    describe('User Login', function () {
+        var apiUrl = /auth/login;
+
+        it('should return "MissingUsername" error trying to login without username', function (done) {
+            var user = {
+                password: '123456'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(user)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('MissingUsername');
+                    done();
+                });
+        });         
+
+        it('should return "MissingPassword" error trying to login without password', function (done) {
+            var user = {
+                username: 'test@salonhelps.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(user)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('MissingPassword');
+                    done();
+                });
         });
 
-        
+        it('should return "LoginFailed" error trying to login wrong password or username', function (done) {
+            var user = {
+                username: 'test@salonhelps.com',
+                password: '123456'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(user)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(403);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('LoginFailed');
+                    done();
+                });
+        });
+
+        it('should return user object trying to login sucessfully', function (done) {
+            var user = {
+                username: 'unittest@gmail.com',
+                password: '123456'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(user)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(409);
+                    res.body.should.have.property('user');
+                    res.body.err.should.have.property('username').eql(user.username);
+                    done();
+                });
+        });
     });
 });
