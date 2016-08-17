700504800887215cdc4986afb3e4774d0c1645ea
Thanh Truong <vagabondtt1503@gmail.com>
2016-08-17 15:38:26 -0400

    add first case in spec

--- SalonCloud.Server/spec/user.spec.js
+++ SalonCloud.Server/spec/user.spec.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var winston = require('winston');
 var configDB = require('./../config/dev/database.js');
 
-describe('Routing', function () {
+describe('User', function () {
     var url = 'http://localhost:3000';
     // within before() you can run all the operations that are needed to setup your tests. In this case
     // I want to create a connection with the database, and when I'm done, I call done().
@@ -14,17 +14,11 @@ describe('Routing', function () {
         mongoose.connect(configDB.url);
         done();
     });
-    // use describe to give a title to your test suite, in this case the tile is "User"
-    // and then specify a function in which we are going to declare all the tests
-    // we want to run. Each test starts with the function it() and as a first argument 
-    // we have to provide a meaningful title for it, whereas as the second argument we
-    // specify a function that takes a single parameter, "done", that we will use 
-    // to specify when our test is completed, and that's what makes easy
-    // to perform async test!
-    describe('User SignUp with Username & Password', function () {
-        var apiUrl = '/auth/signupwithemailandpassword';
+    
+    describe('Create Profile', function () {
+        var apiUrl = '/user/createprofile';
 
-        it('should return "MissingUsername" error trying to register without username', function (done) {
+        it('should return "InvalidTokenError" error trying to create profile with invalid token', function (done) {
             var user = {
                 password: '123456'
             };
@@ -47,3 +41,7 @@ describe('Routing', function () {
                     done();
                 });
         });
+
+        
+    });
+});
