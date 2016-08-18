fdab4a91c0104e44203e6c5153f543eed56be583
Thanh Truong <vagabondtt1503@gmail.com>
2016-08-18 10:16:33 -0400

    add Error list

--- SalonCloud.Server/spec/salonsetting.spec.js
+++ SalonCloud.Server/spec/salonsetting.spec.js
@@ -47,6 +47,35 @@ describe('User', function () {
                 });
         });
 
+        it('should return "InvalidTokenError" error trying to create salon information with invalid token', function (done) {
+            var token = '123456789';
+            var bodyRequest = {
+                'salon_name': 'SunshineNails VA',
+                'address': '2506 Bailey Dr NW, Norcross, GA 30071',
+                'phonenumber': '4049806189',
+                'email': 'salon@salonhelps.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set('Authorization', 'Bearer ' + token)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    } 
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('InvalidTokenError');
+                    done();
+                });
+        });
+
         
     });
 });
