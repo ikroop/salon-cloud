75a64cbf98c6292c71babdd1c8ad9c5d7527fc9e
Thanh Truong <vagabondtt1503@gmail.com>
2016-08-18 22:22:12 -0400

    move ErrorMessage.ts to routes

---
+++ SalonCloud.Server/routes/ErrorMessage.js
@@ -0,0 +1,118 @@
+//Error Messages Definition
+module.exports = {
+    'MissingUsername': {
+        'err': {
+            'name': 'InvalidTokenError',
+            'message': 'Token is invalid'
+        }
+    },
+    'MissingPassword': {
+        'err': {
+            'name': 'MissingPassword',
+            'message': 'A required password is missing!'
+        }
+    },
+    'MissingFullName': {
+        'err': {
+            'name': 'MissingFullName',
+            'message': 'A required FullName is missing!'
+        }
+    },
+    'PasswordTooShort': {
+        'err': {
+            'name': 'PasswordTooShort',
+            'message': 'A password is too short!'
+        }
+    },
+    'NotEmailOrPhoneNumber': {
+        'err': {
+            'name': 'NotEmailOrPhoneNumber',
+            'message': 'Username must be email or phone number!'
+        }
+    },
+    'UsernameAlreadyExists': {
+        'err': {
+            'name': 'UsernameAlreadyExists',
+            'message': 'Username is already existing'
+        }
+    },
+    'InvalidTokenError': {
+        'err': {
+            'name': 'InvalidTokenError',
+            'message': 'Username is already existing'
+        }
+    },
+    'NoPermission': {
+        'err': {
+            'name': 'NoPermission',
+            'message': 'Unauthorized'
+        }
+    },
+    'WrongBirthdayFormat': {
+        'err': {
+            'name': 'WrongBirthdayFormat',
+            'message': 'A birthday is wrong format!'
+        }
+    },
+    'WrongAddressFormat': {
+        'err': {
+            'name': 'WrongAddressFormat',
+            'message': 'A address is wrong format!'
+        }
+    },
+    'WrongSocialSecurityNumberFormat': {
+        'err': {
+            'name': 'WrongSocialSecurityNumberFormat',
+            'message': 'Social Security Number is wrong format!'
+        }
+    },
+    'SalaryRateRangeError': {
+        'err': {
+            'name': 'SalaryRateRangeError',
+            'message': 'Salary rate must be greater than 0 and less than 10'
+        }
+    },
+    'CashRateRangeError': {
+        'err': {
+            'name': 'CashRateRangeError',
+            'message': 'Cash rate must be greater than 0 and less than 10'
+        }
+    },
+    'UserNotFound': {
+        'err': {
+            'name': 'UserNotFound',
+            'message': 'User is not found'
+        }
+    },
+    'SalonNotFound': {
+        'err': {
+            'name': 'SalonNotFound',
+            'message': 'Salon is not found'
+        }
+    },
+    'MissingSalonName': {
+        'err': {
+            'name': 'MissingSalonName',
+            'message': 'A required salonname is missing!'
+        }
+    },
+    'MissingAddress': {
+        'err': {
+            'name': 'MissingAddress',
+            'message': 'A required address is missing!'
+        }
+    },
+    'WrongPhoneNumberFormat': {
+        'err': {
+            'name': 'WrongPhoneNumberFormat',
+            'message': 'Phonenumber format is wrong'
+        }
+    },
+    'WrongEmailFormat': {
+        'err': {
+            'name': 'WrongEmailFormat',
+            'message': 'Email format is wrong'
+        }
+    }
+};
+//# sourceMappingURL=ErrorMessage.js.map
