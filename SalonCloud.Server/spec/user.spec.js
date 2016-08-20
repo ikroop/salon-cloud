93f149e6b19fa27bc902ced9cd8c8e6231999aa6
Phạm Vũ Hoàng Hà <phamha1314@gmail.com>
2016-08-20 21:48:56 +0700

    HaPVH add test case

--- SalonCloud.Server/spec/user.spec.js
+++ SalonCloud.Server/spec/user.spec.js
@@ -52,7 +52,7 @@ describe('User', function () {
             var user = {
                 password: '123456'
             };
-            // once we have specified the info we want to send to the server via POST verb,
+            // once we have specified the info we want to send to the server via GET verb,
             // we need to actually perform the action on the resource, in this case we want to 
             // POST on /api/auth/register and we want to send some info
             // We do this using the request object, requiring supertest!
@@ -67,6 +67,75 @@ describe('User', function () {
                     res.status.should.be.equal(400);
                     res.body.should.have.property('InvalidTokenError');
                     res.body.err.should.have.property('name').eql('Token is invalid');
+                    done();
+                });
+        });
+
+        it('should return "NoPermission" error trying to create profile with no permission', function (done) {
+            var user = {
+                password: '123456'
+            };
+            // once we have specified the info we want to send to the server via GET verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .get(apiUrl)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    } 
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(403);
+                    res.body.should.have.property('NoPermission');
+                    res.body.err.should.have.property('name').eql('Unauthorized');
+                    done();
+                });
+        });
+
+        it('should return "UserNotFound" error trying to create profile with user is not found', function (done) {
+            var user = {
+                password: '123456'
+            };
+            // once we have specified the info we want to send to the server via GET verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .get(apiUrl)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    } 
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('UserNotFound');
+                    res.body.err.should.have.property('name').eql('User is not found');
+                    done();
+                });
+        });
+
+        it('should return "SalonNotFound" error trying to create profile with Salon is not found', function (done) {
+            var user = {
+                password: '123456'
+            };
+            // once we have specified the info we want to send to the server via GET verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .get(apiUrl)
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    } 
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('SalonNotFound');
+                    res.body.err.should.have.property('name').eql('Salon is not found');
                     done();
                 });
         });
