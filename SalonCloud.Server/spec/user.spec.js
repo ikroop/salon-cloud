21834711b00a2c246aa3b332f69c0555266c6b69
Thanh Truong <vagabondtt1503@gmail.com>
2016-08-24 16:50:52 -0400

    add test case to create user profile

--- SalonCloud.Server/spec/user.spec.js
+++ SalonCloud.Server/spec/user.spec.js
@@ -30,7 +30,7 @@ describe('User', function () {
                 }
                 validToken = res.body.auth.token;
                 invalidToken = '1234455';
-                salon_id = '57bb3102b70ec33116d8499e';
+                salon_id = (new Date().getTime()).toString();
                 done();
             });
     });
@@ -70,6 +70,259 @@ describe('User', function () {
                     res.status.should.be.equal(403);
                     res.body.should.have.property('err');
                     res.body.err.should.have.property('name').eql('InvalidTokenError');
+                    done();
+                });
+        });
+
+        it('should return "WrongSSNFormat" error trying to create profile with invalid SSN', function (done) {
+            var token = validToken;
+            var bodyRequest = {
+                "salon_id": salon_id,
+                status: true,
+                role: 3,
+                fullname: 'Jelly Gaskill',
+                nickname: 'Jelly',
+                social_security_number: '1653245',
+                salary_rate: 6.5,
+                cash_rate: 3.5,
+                birthday: 'May, 05',
+                address: '2506 Bailey Dr, Norcross, GA 30071',
+                email: 'legacynails@gmail.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set({ 'Authorization': token })
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('WrongSSNFormat');
+                    done();
+                });
+        });
+
+        it('should return "SalaryRateRangeError" error trying to create profile with salary rate less than 0', function (done) {
+            var token = validToken;
+            var bodyRequest = {
+                "salon_id": salon_id,
+                status: true,
+                role: 3,
+                fullname: 'Jelly Gaskill',
+                nickname: 'Jelly',
+                social_security_number: '165324565',
+                salary_rate: -1,
+                cash_rate: 3.5,
+                birthday: 'May, 05',
+                address: '2506 Bailey Dr, Norcross, GA 30071',
+                email: 'legacynails@gmail.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set({ 'Authorization': token })
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('SalaryRateRangeError');
+                    done();
+                });
+        });
+
+        it('should return "SalaryRateRangeError" error trying to create profile with salary rate greater than 10', function (done) {
+            var token = validToken;
+            var bodyRequest = {
+                "salon_id": salon_id,
+                status: true,
+                role: 3,
+                fullname: 'Jelly Gaskill',
+                nickname: 'Jelly',
+                social_security_number: '165324565',
+                salary_rate: 10.3,
+                cash_rate: 3.5,
+                birthday: 'May, 05',
+                address: '2506 Bailey Dr, Norcross, GA 30071',
+                email: 'legacynails@gmail.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set({ 'Authorization': token })
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('SalaryRateRangeError');
+                    done();
+                });
+        });
+
+        it('should return "CashRateRangeError" error trying to create profile with cash rate less than 0', function (done) {
+            var token = validToken;
+            var bodyRequest = {
+                "salon_id": salon_id,
+                status: true,
+                role: 3,
+                fullname: 'Jelly Gaskill',
+                nickname: 'Jelly',
+                social_security_number: '165324565',
+                salary_rate: 6,
+                cash_rate: -3.5,
+                birthday: 'May, 05',
+                address: '2506 Bailey Dr, Norcross, GA 30071',
+                email: 'legacynails@gmail.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set({ 'Authorization': token })
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('CashRateRangeError');
+                    done();
+                });
+        });
+
+        it('should return "CashRateRangeError" error trying to create profile with cash rate greater than 10', function (done) {
+            var token = validToken;
+            var bodyRequest = {
+                "salon_id": salon_id,
+                status: true,
+                role: 3,
+                fullname: 'Jelly Gaskill',
+                nickname: 'Jelly',
+                social_security_number: '165324565',
+                salary_rate: 6.0,
+                cash_rate: 13.5,
+                birthday: 'May, 05',
+                address: '2506 Bailey Dr, Norcross, GA 30071',
+                email: 'legacynails@gmail.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set({ 'Authorization': token })
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(400);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('CashRateRangeError');
+                    done();
+                });
+        });
+
+        it('should return User Object error trying to create profile successfully', function (done) {
+            var token = validToken;
+            var bodyRequest = {
+                "salon_id": salon_id,
+                status: true,
+                role: 3,
+                fullname: 'Jelly Gaskill',
+                nickname: 'Jelly',
+                social_security_number: '165324565',
+                salary_rate: 6.0,
+                cash_rate: 3.5,
+                birthday: 'May, 05',
+                address: '2506 Bailey Dr, Norcross, GA 30071',
+                email: 'legacynails@gmail.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set({ 'Authorization': token })
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(200);
+                    res.body.should.have.property('profile');
+                    var profile = res.body.profile[0];
+                    profile.should.have.property('salon_id').eql(bodyRequest.salon_id);
+                    done();
+                });
+        });
+
+        it('should return "ProfileAlreadyExist" error trying to create profile with salon_id already exits', function (done) {
+            var token = validToken;
+            var bodyRequest = {
+                "salon_id": salon_id,
+                status: true,
+                role: 3,
+                fullname: 'Jelly Gaskill',
+                nickname: 'Jelly',
+                social_security_number: '165324565',
+                salary_rate: 6.0,
+                cash_rate: 3.5,
+                birthday: 'May, 05',
+                address: '2506 Bailey Dr, Norcross, GA 30071',
+                email: 'legacynails@gmail.com'
+            };
+            // once we have specified the info we want to send to the server via POST verb,
+            // we need to actually perform the action on the resource, in this case we want to 
+            // POST on /api/auth/register and we want to send some info
+            // We do this using the request object, requiring supertest!
+            request(url)
+                .post(apiUrl)
+                .send(bodyRequest)
+                .set({ 'Authorization': token })
+                // end handles the response
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // this is should.js syntax, very clear
+                    res.status.should.be.equal(409);
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('ProfileAlreadyExist');
                     done();
                 });
         });
