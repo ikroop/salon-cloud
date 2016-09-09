3032c590ebd65229d8c51283066e8eadc351c84a
Due Nguyen <duenguyen213@gmail.com>
2016-09-08 20:41:11 -0400

    Add more decorators

--- SalonCloud.Server/core/validation/ValidationDecorators.ts
+++ SalonCloud.Server/core/validation/ValidationDecorators.ts
@@ -113,3 +113,91 @@ export class IsLessThan extends DecoratingValidator {
     }
 }
 
+//Validate if target element is an email.
+//To pass the test: email Regex test returns true.
+export class IsEmail extends DecoratingValidator {
+
+    public errorType: any;
+
+    public IsEmail (wrapedValidator: Validator, errorType: any){
+        this.wrapedValidator = wrapedValidator;
+        this.errorType = errorType;
+    }
+
+    public validatingOperation(){
+        var emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!this.wrapedValidator.targetElement.match(emailReg)) {
+            return this.errorType;
+        }else{
+            return undefined;
+        }
+    }
+}
+
+//Validate if target element is a phone number.
+//To pass the test: phone Regex test returns true.
+export class IsPhoneNumber extends DecoratingValidator {
+
+    public errorType: any;
+
+    public IsPhoneNumber (wrapedValidator: Validator, errorType: any){
+        this.wrapedValidator = wrapedValidator;
+        this.errorType = errorType;
+    }
+
+    public validatingOperation(){
+        var phoneReg = /^\d{10}$/;
+        if (!this.wrapedValidator.targetElement.match(phoneReg)) {
+            return this.errorType;
+        }else{
+            return undefined;
+        }
+    }
+}
+
+//Validate if target element is a social security number.
+//To pass the test: social security number Regex test returns true.
+export class IsSSN extends DecoratingValidator {
+
+    public errorType: any;
+
+    public IsSSN (wrapedValidator: Validator, errorType: any){
+        this.wrapedValidator = wrapedValidator;
+        this.errorType = errorType;
+    }
+
+    public validatingOperation(){
+        var SSNReg = /^\d{9}$/;
+        if (!this.wrapedValidator.targetElement.match(SSNReg)) {
+            return this.errorType;
+        }else{
+            return undefined;
+        }
+    }
+}
+
+//WE CAN USE IsInRange VALIDATOR FOR THIS ONE INSTEAD
+//Validate if target element is a salary rate.
+//To pass the test: target element must be in range of 0 and 10.
+/*export class IsSalaryRate extends DecoratingValidator {
+
+    public errorType: any;
+    public SalaryRate: number;
+
+    public IsSalaryRate (wrapedValidator: Validator, errorType: any, SalaryRate: number){
+        this.wrapedValidator = wrapedValidator;
+        this.errorType = errorType;
+        this.SalaryRate = SalaryRate;
+    }
+
+    public validatingOperation(){
+        if (this.SalaryRate <= 0 || this.SalaryRate >= 10) {
+            return this.errorType;
+        }else{
+            return undefined;
+        }
+    }
+}*/
+
+
+
