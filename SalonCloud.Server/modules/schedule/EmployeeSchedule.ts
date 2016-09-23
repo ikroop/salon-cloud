a9f3b86cd3ffbe74cad9bfb341ec6227e3834d75
duenguyen2103 <duenguyen213@gmail.com>
2016-09-22 23:10:36 -0400

    Add instruction to normalizationWeeklySchedule

--- SalonCloud.Server/modules/schedule/EmployeeSchedule.ts
+++ SalonCloud.Server/modules/schedule/EmployeeSchedule.ts
@@ -23,7 +23,14 @@ export class EmployeeSchedule extends Schedule {
     }
 
     protected normalizeWeeklySchedule(WeeklySchedule: [WeeklyDayData]){
+        //Need to use async/await
         //Todo: implementation >>>> compare with salon to get the best schedules
+        //Step 1: get salon's [WeeklyDayData] (look at getWeeklyScheduleRecord() in parent class for similar implementation);
+        //Step 2: loop through each day in array, logically choose the best schedule for that day;
+        //      ex: On that day, employee schedule status is 'open' but salon schedule status is 'close', we choose 'close'
+        //          On that day, operation time is 10am-6pm for employee but 11am-8pm for salon, we choose '11am-6pm'
+        //          (There are many cases, try to catch all)
+        //Step 3: return the resulted array<WeeklyDayData>
         return WeeklySchedule;
     };
 
