a0ff19f8a9b52b094f1d8cf38c43b64c58fa1037
Due Nguyen <duenguyen213@gmail.com>
2016-09-25 12:15:28 -0400

    # schedule route: add 2 saving routes for employee

--- SalonCloud.Server/routes/schedule.ts
+++ SalonCloud.Server/routes/schedule.ts
@@ -7,6 +7,7 @@ import fs = require('fs');
 import { Router, Request, Response } from "express";
 import {Schedule} from './../modules/schedule/Schedule';
 import {SalonSchedule} from './../modules/schedule/SalonSchedule';
+import {EmployeeSchedule} from './../modules/schedule/EmployeeSchedule'
 
 //import {Validator} from '../core/validator/Validator';
 //import {SalonSchedule} from '../modules/schedule/SalonSchedule';
@@ -25,7 +26,7 @@ export class ScheduleRouter {
     
     getRouter(): Router {
 
-        this.router.post("/saveSalonWeeklySchedule", async(request: Request, response: Response) => {
+        this.router.post("/savesalonweeklyschedule", async(request: Request, response: Response) => {
            
             //Todo: call Salon (and employee if needed) static validation
             //Todo: build a factory for schedule;
@@ -33,13 +34,29 @@ export class ScheduleRouter {
             let test  = await testObject.saveWeeklySchedule(request.body.weekly_schedules);
             response.status(200).json(test)
         });
-        this.router.post("/saveSalonDailySchedule", async(request: Request, response: Response) => {
+        this.router.post("/savesalondailyschedule", async(request: Request, response: Response) => {
            
             //Todo: call Salon (and employee if needed) static validation
             //Todo: build a factory for schedule;
             let testObject = new SalonSchedule(request.body.salon_id);
-            let test  = await testObject.saveDailySchedule(request.body.daily_schedules);
+            let returnResponse  = await testObject.saveDailySchedule(request.body.daily_schedule);
+            response.status(200).json(returnResponse)
+        });
+        this.router.post("/saveemployeeweeklyschedule", async(request: Request, response: Response) => {
+           
+            //Todo: call Salon (and employee if needed) static validation
+            //Todo: build a factory for schedule;
+            let scheduleObject = new EmployeeSchedule(request.body.salon_id, request.body.employee_id);
+            let test  = await scheduleObject.saveWeeklySchedule(request.body.weekly_schedules);
             response.status(200).json(test)
+        });
+        this.router.post("/saveemployeedailyschedule", async(request: Request, response: Response) => {
+           
+            //Todo: call Salon (and employee if needed) static validation
+            //Todo: build a factory for schedule;
+            let scheduleObject = new EmployeeSchedule(request.body.salon_id, request.body.employee_id);
+            let returnResponse  = await scheduleObject.saveDailySchedule(request.body.daily_schedule);
+            response.status(200).json(returnResponse)
         });
         this.router.post("/testpost", async(request: Request, response: Response) => {
             response.status(200).json({"testpost":"OK"});
