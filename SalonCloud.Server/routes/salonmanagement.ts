837efc6b8ab461af2eab8189b521990c21228b45
Thanh Truong <vagabondtt1503@gmail.com>
2016-09-28 23:47:34 -0400

    add route template

---
+++ SalonCloud.Server/routes/salonmanagement.ts
@@ -0,0 +1,35 @@
+﻿/*
+ * Salon REST API
+ */
+
+import { Router, Request, Response } from "express";
+import { AuthorizationRouter } from "./authorization";
+import { Authentication } from '../core/authentication/authentication';
+
+export class SalonManagementRouter {
+    private router: Router = Router();
+
+    getRouter(): Router {
+        var authentication = new Authentication();
+        var authorizationRouter = new AuthorizationRouter();
+
+        this.router.post("/create", authorizationRouter.checkPermission, function (request: Request, response: Response) {
+            
+        });
+
+        this.router.post("/getsalonlist", authorizationRouter.checkPermission, function (request: Request, response: Response) {
+           
+        });
+
+        this.router.post("/updatesettings", authorizationRouter.checkPermission, function (request: Request, response: Response) {
+           
+        });
+
+        this.router.post("/updateinformation", authorizationRouter.checkPermission, function (request: Request, response: Response) {
+           
+        });
+
+        return this.router;
+
+    }
+}
