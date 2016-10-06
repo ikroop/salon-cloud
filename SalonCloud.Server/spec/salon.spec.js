91c5d6f7e727faba80600d3668e9a009143e341d
Thanh Truong <vagabondtt1503@gmail.com>
2016-10-05 22:28:30 -0400

    remove comment

--- SalonCloud.Server/spec/salon.spec.js
+++ SalonCloud.Server/spec/salon.spec.js
@@ -3,29 +3,23 @@ var assert = require('assert');
 var request = require('supertest');
 var mongoose = require('mongoose');
 var winston = require('winston');
-var configDB = require('./../config/dev/database.js');
 
-describe('Salon', function () {
+describe('Salon Management', function () {
     var url = 'http://localhost:3000';
     var validToken;
     var invalidToken;
     var defaultPassword = '1234@1234'
 
-    // within before() you can run all the operations that are needed to setup your tests. In this case
-    // I want to create a connection with the database, and when I'm done, I call done().
     before(function (done) {
-        // In our tests we use the test db
-        mongoose.connect(configDB.url);
 
-        //get valid token
+        // Login and get token
         var user = {
             username: 'unittest1472245629435@gmail.com',
             password: defaultPassword
         };
         request(url)
             .post('/auth/signinwithemailandpassword')
-            .send(user)
-            // end handles the response
+            .send(user)            
             .end(function (err, res) {
                 if (err) {
                     throw err;
@@ -36,8 +30,8 @@ describe('Salon', function () {
             });
     });
 
-    describe('Create Salon Information', function () {
-        var apiUrl = '/salon/createinformation';
+    describe('Unit Test Create Salon API', function () {
+        var apiUrl = '/api/v1/salon/create';
 
         it('should return "InvalidTokenError" error trying to create salon information with invalid token', function (done) {
             var token = invalidToken;
@@ -47,20 +41,16 @@ describe('Salon', function () {
                 'phonenumber': '4049806189',
                 'email': 'salon@salonhelps.com'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
             request(url)
                 .post(apiUrl)
                 .send(bodyRequest)
                 .set({ 'Authorization': token })
-                // end handles the response
+                
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
+                    
                     res.status.should.be.equal(403);
                     res.body.should.have.property('err');
                     res.body.err.should.have.property('name').eql('InvalidTokenError');
@@ -75,20 +65,16 @@ describe('Salon', function () {
                 'phonenumber': '4049806189',
                 'email': 'salon@salonhelps.com'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
             request(url)
                 .post(apiUrl)
                 .send(bodyRequest)
                 .set({ 'Authorization': token })
-                // end handles the response
+                
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
+                    
                     res.status.should.be.equal(400);
                     res.body.should.have.property('err');
                     res.body.err.should.have.property('name').eql('MissingSalonName');
@@ -103,20 +89,16 @@ describe('Salon', function () {
                 'phonenumber': '4049806189',
                 'email': 'salon@salonhelps.com'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
             request(url)
                 .post(apiUrl)
                 .send(bodyRequest)
                 .set({ 'Authorization': token })
-                // end handles the response
+                
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
+                    
                     res.status.should.be.equal(400);
                     res.body.should.have.property('err');
                     res.body.err.should.have.property('name').eql('MissingAddress');
@@ -124,7 +106,7 @@ describe('Salon', function () {
                 });
         });
 
-        /*it('should return "WrongAddressFormat" error trying to create salon information with wrong address format', function (done) {
+        it('should return "WrongAddressFormat" error trying to create salon information with wrong address format', function (done) {
             var token = validToken;
             var bodyRequest = {
                 'salon_name': 'SunshineNails VA',
@@ -132,26 +114,22 @@ describe('Salon', function () {
                 'phonenumber': '4049806189',
                 'email': 'salon@salonhelps.com'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
+   
             request(url)
                 .post(apiUrl)
                 .send(bodyRequest)
                 .set('Authorization', token)
-                // end handles the response
+                
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
                     res.status.should.be.equal(400);
-                    //res.body.should.have.property('err');
-                    //res.body.err.should.have.property('name').eql('WrongAddressFormat');
+                    res.body.should.have.property('err');
+                    res.body.err.should.have.property('name').eql('WrongAddressFormat');
                     done();
                 });
-        });*/
+        });
 
         it('should return "MissingPhoneNumber" error trying to create salon information without phone number', function (done) {
             var token = validToken;
@@ -160,20 +138,16 @@ describe('Salon', function () {
                 'address': '2506 Bailey Dr NW, Norcross, GA 30071',
                 'email': 'salon@salonhelps.com'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
             request(url)
                 .post(apiUrl)
                 .set({ 'Authorization': token })
                 .send(bodyRequest)
-                // end handles the response
+                
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
+                    
                     res.status.should.be.equal(400);
                     res.body.should.have.property('err');
                     res.body.err.should.have.property('name').eql('MissingPhoneNumber');
@@ -189,20 +163,16 @@ describe('Salon', function () {
                 'phonenumber': '1234',
                 'email': 'salon@salonhelps.com'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
             request(url)
                 .post(apiUrl)
                 .send(bodyRequest)
                 .set({ 'Authorization': token })
-                // end handles the response
+                
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
+                    
                     res.status.should.be.equal(400);
                     res.body.should.have.property('err');
                     res.body.err.should.have.property('name').eql('WrongPhoneNumberFormat');
@@ -218,20 +188,15 @@ describe('Salon', function () {
                 'phonenumber': '4049806189',
                 'email': 'salon@salonhe'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
             request(url)
                 .post(apiUrl)
                 .send(bodyRequest)
                 .set({ 'Authorization': token })
-                // end handles the response
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
+                    
                     res.status.should.be.equal(400);
                     res.body.should.have.property('err');
                     res.body.err.should.have.property('name').eql('WrongEmailFormat');
@@ -247,20 +212,16 @@ describe('Salon', function () {
                 'phonenumber': '4049806189',
                 'email': 'salon@salonhelps.com'
             };
-            // once we have specified the info we want to send to the server via POST verb,
-            // we need to actually perform the action on the resource, in this case we want to 
-            // POST on /api/auth/register and we want to send some info
-            // We do this using the request object, requiring supertest!
             request(url)
                 .post(apiUrl)
                 .send(bodyRequest)
                 .set({ 'Authorization': token })
-                // end handles the response
+                
                 .end(function (err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
+                    
                     res.status.should.be.equal(200);
                     res.body.should.have.property('_id');
                     done();
