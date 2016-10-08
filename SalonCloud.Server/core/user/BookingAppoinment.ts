e480d46997c323aca716aa54926924ba03b26261
duenguyen2103 <duenguyen213@gmail.com>
2016-10-08 15:27:40 -0400

    smoothing

--- SalonCloud.Server/core/user/BookingAppoinment.ts
+++ SalonCloud.Server/core/user/BookingAppoinment.ts
@@ -5,7 +5,7 @@ import {SalonCloudResponse} from './../SalonCloudResponse'
 import {AppointmentData} from './../../modules/appointmentManagement/AppointmentData'
 import {AppointmentManagement} from './../../modules/appointmentManagement/AppointmentManagement'
 import {ServiceManagement} from './../../modules/serviceManagement/ServiceManagement'
-import {ServiceItem} from './../../modules/serviceManagement/ServiceItem'
+import {ServiceItemData} from './../../modules/serviceManagement/ServiceData'
 import {Verification} from './../verification/Verification'
 
 
@@ -20,12 +20,12 @@ export class BookingAppointment implements BookingAppointmentBehavior {
         return;
     };
     
-    getAvailableTime(date : Date, services : Array<ServiceItem>) : SalonCloudResponse<BookingSchedule>{
+    getAvailableTime(date : Date, services : Array<ServiceItemData>) : SalonCloudResponse<BookingSchedule>{
 
         return;
     };
 
-    getServices() : SalonCloudResponse<Array<ServiceItem>>{
+    getServices() : SalonCloudResponse<Array<ServiceItemData>>{
 
         return;
     };
