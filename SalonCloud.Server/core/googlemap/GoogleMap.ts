d9770abcc18a74f0f984249b48b8cbb94dfcca03
Thanh Truong <vagabondtt1503@gmail.com>
2016-10-08 12:14:17 -0400

    add google maps

--- SalonCloud.Server/core/googlemap/GoogleMap.ts
+++ SalonCloud.Server/core/googlemap/GoogleMap.ts
@@ -1,8 +1,15 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ */
 var googleMapsClient = require('@google/maps').createClient();
 
 export class GoogleMap {
     static async getTimeZone(address: string) {
         var geocode: any = undefined;
+        var timezone: any = undefined;
         await googleMapsClient.geocode({
             address: address
         }, function (err, response) {
@@ -10,5 +17,15 @@ export class GoogleMap {
                 console.log(response.json.results);
             }
         });
+
+        await googleMapsClient.timezone({
+            location: [geocode.lat, geocode.long],
+            timestamp: 1331766000,
+            language: 'en'
+        }, function (err, response) {
+            timezone = response.json;
+        });
+
+        return timezone;
     }
 }
