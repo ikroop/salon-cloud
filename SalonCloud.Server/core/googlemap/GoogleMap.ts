4253e748aa70f8460c6e369dd3da0b219c9e6d8e
Thanh Truong <vagabondtt1503@gmail.com>
2016-10-12 17:36:09 -0400

    comment code

--- SalonCloud.Server/core/googlemap/GoogleMap.ts
+++ SalonCloud.Server/core/googlemap/GoogleMap.ts
@@ -8,17 +8,24 @@ var googleMapsClient = require('@google/maps').createClient({
     key: 'AIzaSyCptaA0X0FDTsjVgotaKtGgbPYjXBszGgk'
 });
 export class GoogleMap {
+
+    /**
+	*@name: getTimeZone
+    *@parameter: address: string
+    *@return: timezone object
+	*/
     static async getTimeZone(address: string) {
         var geocode: any = undefined;
         var timezone: any = undefined;
 
         let promise = new Promise(function(resolve, reject) {
-            googleMapsClient.geocode({
+            googleMapsClient.geocode({ // get geocode
                 address: address
             }, function(err, response) {
                 if (!err) {
                     geocode = response.json.results[0].geometry.location;
-                    timezone = googleMapsClient.timezone({
+
+                    googleMapsClient.timezone({ // get timezone
                         location: [geocode.lat, geocode.lng]
                     }, function(err, response) {
                         if (!err) {
@@ -28,6 +35,7 @@ export class GoogleMap {
                             resolve(err);
                         }
                     });
+                    
                 } else {
                     resolve(err);
                 }
