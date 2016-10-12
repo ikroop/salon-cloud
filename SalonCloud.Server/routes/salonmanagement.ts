965a12d4c0269c75f6920bfeec5c442d86bc8739
Thanh Truong <vagabondtt1503@gmail.com>
2016-10-11 21:03:38 -0400

    format code

--- SalonCloud.Server/routes/salonmanagement.ts
+++ SalonCloud.Server/routes/salonmanagement.ts
@@ -5,10 +5,10 @@
 import { Router, Request, Response } from "express";
 import { AuthorizationRouter } from "./authorization";
 import { Authentication } from '../core/authentication/authentication';
-import {SignedInUser} from '../core/user/SignedInUser';
-import {SalonManagement} from '../modules/salonManagement/SalonManagement';
-import {UserManagement} from '../modules/userManagement/UserManagement';
-import {SalonInformation} from '../modules/salonManagement/SalonData'
+import { SignedInUser } from '../core/user/SignedInUser';
+import { SalonManagement } from '../modules/salonManagement/SalonManagement';
+import { UserManagement } from '../modules/userManagement/UserManagement';
+import { SalonInformation } from '../modules/salonManagement/SalonData'
 
 export class SalonManagementRouter {
     private router: Router = Router();
@@ -17,9 +17,9 @@ export class SalonManagementRouter {
         var authentication = new Authentication();
         var authorizationRouter = new AuthorizationRouter();
 
-        this.router.post("/create", authorizationRouter.checkPermission, async(request: Request, response: Response) => {
+        this.router.post("/create", authorizationRouter.checkPermission, async (request: Request, response: Response) => {
             var userObject = new SignedInUser(new SalonManagement(), new UserManagement(request.user._id));
-            var salonInformationInput : SalonInformation = {
+            var salonInformationInput: SalonInformation = {
                 email: request.body.email,
                 phone: {
                     number: request.body.phonenumber,
@@ -31,38 +31,38 @@ export class SalonManagementRouter {
                 },
                 salon_name: request.body.salon_name,
             }
-            
+
             var salonCreation = await userObject.createSalon(salonInformationInput);
             var dataReturn;
-            if(salonCreation.err){
-                dataReturn ={
+            if (salonCreation.err) {
+                dataReturn = {
                     'err': salonCreation.err,
                 }
-            }else{
-            dataReturn = {
-                'err': salonCreation.err,
-                'uid': salonCreation.data.uid,
-                'salon_id': salonCreation.data.salon_id,
-                'role': salonCreation.data.role,
-                'default_schedule': salonCreation.data.default_schedule,
-                'sample_services': salonCreation.data.sample_services,
-                'salon_data': salonCreation.data.salon_data,
-            }
+            } else {
+                dataReturn = {
+                    'err': salonCreation.err,
+                    'uid': salonCreation.data.uid,
+                    'salon_id': salonCreation.data.salon_id,
+                    'role': salonCreation.data.role,
+                    'default_schedule': salonCreation.data.default_schedule,
+                    'sample_services': salonCreation.data.sample_services,
+                    'salon_data': salonCreation.data.salon_data,
+                }
             }
             response.status(salonCreation.code);
             response.json(dataReturn);
         });
 
         this.router.post("/getsalonlist", authorizationRouter.checkPermission, function (request: Request, response: Response) {
-           
+
         });
 
         this.router.post("/updatesettings", authorizationRouter.checkPermission, function (request: Request, response: Response) {
-           
+
         });
 
         this.router.post("/updateinformation", authorizationRouter.checkPermission, function (request: Request, response: Response) {
-           
+
         });
 
         return this.router;
