f2235b756189baf8c791c25294a25214e0120a03
Thanh Truong <vagabondtt1503@gmail.com>
2016-10-14 17:17:41 -0400

    fix typo

--- SalonCloud.Server/core/verification/ByPhoneVerification.ts
+++ SalonCloud.Server/core/verification/ByPhoneVerification.ts
@@ -11,7 +11,7 @@ export class ByPhoneVerification extends Verification {
             err: undefined,
             data: undefined
         }
-        var response.err = await Twilio.sendSMS(username, content);
+        response.err = await Twilio.sendSMS(username, content);
         return response;
     }
 }
