a36dd4cd9af294915faeb2e78c55b36e40fab9d3
Due Nguyen <duenguyen213@gmail.com>
2016-10-19 14:20:10 -0400

    #21 class name changing from PhoneAppointment to BookingAppointment

--- SalonCloud.Server/modules/appointmentManagement/BookingAppointment.ts
+++ SalonCloud.Server/modules/appointmentManagement/BookingAppointment.ts
@@ -8,7 +8,7 @@ import {AppointmentAbstract} from './AppointmentAbstract'
 import {AppointmentData} from './AppointmentData'
 import {SalonCloudResponse} from './../../core/SalonCloudResponse'
 
-export class PhoneCallAppointment extends AppointmentAbstract {
+export class BookingAppointment extends AppointmentAbstract {
 
     protected validation(appointment : AppointmentData) : SalonCloudResponse<string>{
 
