ed154ee53c69836fc6212344dead15695c2caef5
Due Nguyen <duenguyen213@gmail.com>
2016-10-19 14:19:39 -0400

    #21 add addCustomerProfile method to CustomerManagement

--- SalonCloud.Server/modules/userManagement/CustomerManagement.ts
+++ SalonCloud.Server/modules/userManagement/CustomerManagement.ts
@@ -1,24 +1,52 @@
 
 
-import {UserData, UserProfile} from './UserData'
-import {CustomerManagementBehavior} from './CustomerManagementBehavior'
+import { UserData, UserProfile } from './UserData'
+import { CustomerManagementBehavior } from './CustomerManagementBehavior'
+import { UserManagement } from './UserManagement'
+import {SalonCloudResponse} from './../../core/SalonCloudResponse'
 
 
-export class CustomerManagement implements CustomerManagementBehavior{
+export class CustomerManagement extends UserManagement implements CustomerManagementBehavior {
 
-    addCustomer(phone, profile :UserProfile) : boolean{
+    public async addCustomerProfile(customerId: string, profile: any): Promise<SalonCloudResponse<UserProfile>> {
+        var returnResult: SalonCloudResponse<UserProfile> = {
+            code: undefined,
+            data: undefined,
+            err: undefined
+        }
+        var newProfile : UserProfile = {
+            role: 4,
+            address: profile.address,
+            birthday: profile.birthday,
+            salon_id: this.salonId,
+            salary_rate: 0,
+            cash_rate: 0,
+            fullname: profile.customer_name,
+            nickname: profile.customer_name,
+            social_security_number: undefined,
+            status: true
+        }
+        var addProfileAction = await this.addProfile(customerId, newProfile);
+        if (addProfileAction.err) {
+            returnResult.err = addProfileAction.err;
+            returnResult.code = addProfileAction.code;
+            return returnResult;
+        } else {
+            returnResult.code = addProfileAction.code;
+            returnResult.data = addProfileAction.data;
+            return returnResult;
+        }    
+    };
+
+    getAllCustomers(): Array<UserData> {
         return;
     };
 
-    getAllCustomers() : Array<UserData>{
+    getCustomer(customerId: string): UserData {
         return;
     };
 
-    getCustomer(customerId : string) : UserData{
-        return;
-    };
-
-    updateCustomer(customerId : string, profile :UserProfile) : boolean{
+    updateCustomer(customerId: string, profile: UserProfile): boolean {
         return;
     };
 }
