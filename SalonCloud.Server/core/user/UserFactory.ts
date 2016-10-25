064c4d1c6abed07888260b34a3e4a6cddb8d5316
Due Nguyen <duenguyen213@gmail.com>
2016-10-25 16:57:35 -0400

    #21 admin factory and router

---
+++ SalonCloud.Server/core/user/UserFactory.ts
@@ -0,0 +1,18 @@
+
+
+import { Owner } from './Owner'
+import { Manager } from './Manager'
+import { Technician } from './Technician'
+import { Customer } from './Customer'
+import { SalonManagement } from './../../modules/salonManagement/SalonManagement'
+import {AdministratorBehavior} from './AdministratorBehavior'
+
+export class UserFactory {
+    public static createAdminUserObject(userId: string, salonId: string, role: number) : AdministratorBehavior {
+        if(role==1){
+            return new Owner(userId, new SalonManagement(salonId));
+        }else if(role==2){
+            return new Manager(userId, new SalonManagement(salonId));
+        }
+    }
+}
--- SalonCloud.Server/routes/appointmentmanagement.ts
+++ SalonCloud.Server/routes/appointmentmanagement.ts
@@ -8,7 +8,9 @@ import { Authentication } from '../core/authentication/authentication';
 import { Authorization } from "../core/authorization/authorization";
 import { AuthorizationRouter } from "./authorization";
 import { AdministratorBehavior } from "./../core/user/AdministratorBehavior";
+import {AbstractAdministrator} from './../core/user/AbstractAdministrator'
 import { AppointmentData } from "./../modules/appointmentManagement/AppointmentData";
+import { UserFactory } from './../core/user/UserFactory';
 
 export class AppointmentManagementRouter {
     private router: Router = Router();    
@@ -18,20 +20,29 @@ export class AppointmentManagementRouter {
         var authorizationRouter = new AuthorizationRouter();
        
         this.router.post("/bookbyphone", function (request: Request, response: Response) {
-            var admin:AdministratorBehavior;
+            var admin: AdministratorBehavior;
 
             // User Factory get Owner or Manager by Id
             // TODO
+            admin = UserFactory.createAdminUserObject(request.user._id, request.body.salon_id, request.user.role);
 
 
             // Get data for request.body
-            var appointment:AppointmentData;
+            var appointment = request.body;
+
 
             // call create appointment function
-            admin.saveAppointment(appointment);
+            var result = admin.saveAppointment(appointment);
 
             //return data
+            var responseData;
+            if(result.err){
+                responseData = result.err;
+            }else{
+                responseData = result.data;
+            }
 
+            response.status(result.code).json(responseData);
 
         });
         return this.router;
