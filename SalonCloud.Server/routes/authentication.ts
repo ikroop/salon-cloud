6f96cabb9308921e46383c9f32d8ecbc21dd0cb3
Thanh Truong <vagabondtt1503@gmail.com>
2016-10-27 23:37:02 -0400

    add data type

--- SalonCloud.Server/routes/authentication.ts
+++ SalonCloud.Server/routes/authentication.ts
@@ -29,7 +29,7 @@ export class AuthenticationRouter {
 
         this.router.post("/signinwithusernameandpassword", authorizationRouter.checkPermission, async function (request: Request, response: Response) {
             //TODO: have to use Anonymouse class
-            let result = await authentication.signInWithUsernameAndPassword(request.body.username, request.body.password);
+            let result:any = await authentication.signInWithUsernameAndPassword(request.body.username, request.body.password);
             response.statusCode = result.code;
             if(result.err){
                 response.json(result.err);
