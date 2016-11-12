ecdeefbdc440a1c8d730c9dd95270dce5aac354f
Thanh Truong <vagabondtt1503@gmail.com>
2016-11-12 17:11:31 -0500

    remove callback parameter

--- src/Core/Authentication/AuthenticationBehavior.ts
+++ src/Core/Authentication/AuthenticationBehavior.ts
@@ -7,9 +7,9 @@
 
 import { SalonCloudResponse } from './../SalonCloudResponse';
 export interface AuthenticationBehavior {
-    changePassword(oldPasswords: string, newPassword: string, code: string, callback);
-    sendVerifyCode(username: string, callback);
-    signInWithUsernameAndPassword(username: string, password: string, callback);
-    signUpWithUsernameAndPassword(username: string, password: string, callback);
-    verifyToken(token: string, callback);
+    changePassword(oldPasswords: string, newPassword: string, code: string);
+    sendVerifyCode(username: string);
+    signInWithUsernameAndPassword(username: string, password: string);
+    signUpWithUsernameAndPassword(username: string, password: string);
+    verifyToken(token: string);
 }
