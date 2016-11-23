4830474718af62738fb146ccb41dea51730ab302
Due Nguyen <duenguyen213@gmail.com>
2016-11-23 17:37:12 -0500

    add route for saveEmployeeWeeklySchedule

--- src/Routes/Schedule.ts
+++ src/Routes/Schedule.ts
@@ -22,12 +22,9 @@ export class ScheduleRouter {
 
         this.router.post('/savesalonweeklyschedule', async(request: Request, response: Response) => {
            
-            //Todo: call Salon (and employee if needed) static validation
-            //Todo: build a factory for schedule;
             var admin: AdministratorBehavior;
-            console.log(request.user);
-            // User Factory get Owner or Manager by Id
-            // TODO
+
+            //create appropriate user object using UserFactory;
             admin = UserFactory.createAdminUserObject(request.user._id, request.body.salon_id, request.user.role);
             var weeklySchedule : WeeklyScheduleData = {
                 salon_id: request.body.salon_id,
@@ -59,11 +56,31 @@ export class ScheduleRouter {
         });
         this.router.post('/saveemployeeweeklyschedule', async(request: Request, response: Response) => {
            
-            //Todo: call Salon (and employee if needed) static validation
-            //Todo: build a factory for schedule;
+            var admin: AdministratorBehavior;
+
+            //create appropriate user object using UserFactory;
+            admin = UserFactory.createAdminUserObject(request.user._id, request.body.salon_id, request.user.role);
+            var weeklySchedule : WeeklyScheduleData = {
+                salon_id: request.body.salon_id,
+                employee_id: request.body.employee_id,
+                week: request.body.weekly_schedules
+            };
+            let result = await admin.updateWeeklySchedule(request.body.employee_id, weeklySchedule);
+
+            var responseData;
+            if(result.err){
+                responseData = result.err;
+            }else{
+                responseData = result.data;
+            }
+            response.status(result.code).json(responseData);
+
+
+            /*
             let scheduleObject = new EmployeeSchedule(request.body.salon_id, request.body.employee_id);
             let test  = await scheduleObject.saveWeeklySchedule(request.body.weekly_schedules);
             response.status(200).json(test)
+            */
         });
         this.router.post('/saveemployeedailyschedule', async(request: Request, response: Response) => {
            
