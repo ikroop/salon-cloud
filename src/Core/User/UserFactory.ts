6bc4276565d12acd74bb3c2f3abd5d52d9ee8930
Thanh Truong <vagabondtt1503@gmail.com>
2016-12-09 11:21:37 -0500

    change from if..else to switch case

--- src/Core/User/UserFactory.ts
+++ src/Core/User/UserFactory.ts
@@ -9,14 +9,24 @@ import { Manager } from './Manager'
 import { Technician } from './Technician'
 import { Customer } from './Customer'
 import { SalonManagement } from './../../Modules/SalonManagement/SalonManagement'
-import {AdministratorBehavior} from './AdministratorBehavior'
+import { AdministratorBehavior } from './AdministratorBehavior'
+import { RoleDefinition } from '../../Core/Authorization/RoleDefinition'
 
 export class UserFactory {
-    public static createAdminUserObject(userId: string, salonId: string, role: string) : AdministratorBehavior {
-        if(role==='Owner'){
-            return new Owner(userId, new SalonManagement(salonId));
-        }else if(role=='Manager'){
-            return new Manager(userId, new SalonManagement(salonId));
+    public static createAdminUserObject(userId: string, salonId: string, role: string): AdministratorBehavior {
+        let admin: AdministratorBehavior = undefined;
+        switch (role) {
+            case RoleDefinition.Owner.text:
+                admin = new Owner(userId, new SalonManagement(salonId));
+                break;
+            case RoleDefinition.Manager.text:
+                admin = new Manager(userId, new SalonManagement(salonId));
+                break;
+            default:
+                admin = undefined;
+                break;
         }
+
+        return admin;
     }
 }
