9122682690d0cc260961d0b3c2a2c39f92bf1d2e
Thanh Truong <vagabondtt1503@gmail.com>
2016-12-08 23:39:53 -0500

    specify return data

--- src/Core/User/AdministratorBehavior.ts
+++ src/Core/User/AdministratorBehavior.ts
@@ -8,6 +8,7 @@ import { UserProfile } from './../../Modules/UserManagement/UserData'
 import { AppointmentData } from './../../Modules/AppointmentManagement/AppointmentData'
 import { IDailyScheduleData, IWeeklyScheduleData, WeeklyScheduleData, DailyScheduleData } from './../../Modules/Schedule/ScheduleData';
 import { ScheduleBehavior } from './../../Modules/Schedule/ScheduleBehavior'
+import { SalonCloudResponse } from './../SalonCloudResponse'
 
 export interface AdministratorBehavior {
     cancelAppointment(appointmentId: string);
@@ -17,6 +18,6 @@ export interface AdministratorBehavior {
     getEmployeeProfile(employeeId: string): UserProfile;
     saveAppointment(appointment: AppointmentData);
     updateAppointment(appointment: AppointmentData);
-    updateDailySchedule(employeeId: string, dailySchedule: DailyScheduleData, schedule: ScheduleBehavior);
-    updateWeeklySchedule(employeeId: string, weeklySchedule: WeeklyScheduleData, schedule: ScheduleBehavior);
+    updateDailySchedule(employeeId: string, dailySchedule: DailyScheduleData, schedule: ScheduleBehavior) : Promise<SalonCloudResponse<IDailyScheduleData>>;
+    updateWeeklySchedule(employeeId: string, weeklySchedule: WeeklyScheduleData, schedule: ScheduleBehavior) : Promise<SalonCloudResponse<IWeeklyScheduleData>>;
 }
