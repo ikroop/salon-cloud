8443813157def18cc6c37700bb0263ac0d12eb00
Due Nguyen <duenguyen213@gmail.com>
2016-12-11 14:11:00 -0500

    add validations to appointment creation

--- src/Modules/AppointmentManagement/BookingAppointment.ts
+++ src/Modules/AppointmentManagement/BookingAppointment.ts
@@ -74,23 +74,23 @@ export class BookingAppointment extends AppointmentAbstract {
             response.code = 400;
             return response;
         }
-        
+
 
         var appointmentItemsArrayValidator = new BaseValidator(appointment.appointment_items);
         appointmentItemsArrayValidator = new MissingCheck(appointmentItemsArrayValidator, ErrorMessage.MissingAppointmentItemsArray);
         var appointmentItemSArrayError = await appointmentItemsArrayValidator.validate();
-        if (appointmentItemSArrayError){
+        if (appointmentItemSArrayError) {
             response.err = appointmentItemSArrayError.err;
             response.code = 400;
             return response;
         }
 
-        for(var eachItem of appointment.appointment_items){
+        for (var eachItem of appointment.appointment_items) {
             let employeeIdValidator = new BaseValidator(eachItem.employee_id);
             employeeIdValidator = new MissingCheck(employeeIdValidator, ErrorMessage.MissingEmployeeId);
             employeeIdValidator = new IsValidSalonId(employeeIdValidator, ErrorMessage.EmployeeNotFound);
             let employeeIdError = await employeeIdValidator.validate();
-            if(employeeIdError){
+            if (employeeIdError) {
                 response.err = employeeIdError.err;
                 response.code = 400;
                 return response;
@@ -100,7 +100,7 @@ export class BookingAppointment extends AppointmentAbstract {
             startValidator = new MissingCheck(startValidator, ErrorMessage.MissingStartDate);
             startValidator = new IsSalonTime(startValidator, ErrorMessage.InvalidDate)
             let startError = await startValidator.validate();
-            if(startError){
+            if (startError) {
                 response.err = startError.err;
                 response.code = 400;
                 return response;
@@ -110,20 +110,81 @@ export class BookingAppointment extends AppointmentAbstract {
             endValidator = new MissingCheck(endValidator, ErrorMessage.MissingEndDate);
             endValidator = new IsSalonTime(endValidator, ErrorMessage.InvalidDate)
             let endError = await endValidator.validate();
-            if(endError){
+            if (endError) {
                 response.err = endError.err;
                 response.code = 400;
                 return response;
             }
-            
+
             let serviceValidator = new BaseValidator(eachItem.service);
             serviceValidator = new MissingCheck(serviceValidator, ErrorMessage.MissingServiceItem);
             let serviceError = await serviceValidator.validate();
-            if(serviceError){
+            if (serviceError) {
                 response.err = serviceError.err;
                 response.code = 400;
                 return response;
             }
+
+            let serviceIdValidator = new BaseValidator(eachItem.service.service_id);
+            serviceValidator = new MissingCheck(serviceIdValidator, ErrorMessage.MissingServiceId);
+            let serviceIdError = await serviceIdValidator.validate();
+            if (serviceIdError) {
+                response.err = serviceIdError.err;
+                return response;
+            }
+
+            let serviceNameValidator = new BaseValidator(eachItem.service.service_name);
+            serviceValidator = new MissingCheck(serviceNameValidator, ErrorMessage.MissingServiceName);
+            let serviceNameError = await serviceNameValidator.validate();
+            if (serviceNameError) {
+                response.err = serviceNameError.err;
+                return response;
+            }
+
+            let priceValidator = new BaseValidator(eachItem.service.price);
+            serviceValidator = new MissingCheck(priceValidator, ErrorMessage.MissingServicePrice);
+            let priceError = await priceValidator.validate();
+            if (priceError) {
+                response.err = priceError.err;
+                return response;
+            }
+
+            let serviceTimeValidator = new BaseValidator(eachItem.service.price);
+            serviceValidator = new MissingCheck(serviceTimeValidator, ErrorMessage.MissingServicePrice);
+            let serviceTimeError = await serviceTimeValidator.validate();
+            if(serviceTimeError){
+                response.err = serviceTimeError.err;
+                return response;
+            }
+
+
+            let overLapValidator = new BaseValidator(eachItem.overlapped);
+            overLapValidator = new MissingCheck(overLapValidator, ErrorMessage.MissingOverlap);
+            let overLapError = await overLapValidator.validate();
+            if (overLapError) {
+                response.err = overLapError.err;
+                response.code = 400;
+                return response;
+            }
+
+            let overlapStatusValidator = new BaseValidator(eachItem.overlapped.status);
+            overlapStatusValidator = new MissingCheck(overlapStatusValidator, ErrorMessage.MissingOverlapStatus);
+            let overlapStatusError = await overlapStatusValidator.validate();
+            if(overlapStatusError){
+                response.err = overlapStatusError.err;
+                response.code = 400;
+                return response;
+            }
+
+            let overlapAppointmentIdValidator = new BaseValidator(eachItem.overlapped.overlapped_appointment_id);
+            overlapAppointmentIdValidator = new MissingCheck(overlapAppointmentIdValidator, ErrorMessage.MissingAppointmentId);
+            let overlapAppointmentIdError = await overlapAppointmentIdValidator.validate();
+            if(overlapAppointmentIdError){
+                response.err = overlapAppointmentIdError.err;
+                response.code = 500;
+                return response;
+            }
+
         }
         response.code = 200;
         return response;
