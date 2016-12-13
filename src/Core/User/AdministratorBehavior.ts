93f5e58f1931ebc098e0a692852dcb726a4df115
Due Nguyen <duenguyen213@gmail.com>
2016-12-13 00:16:21 -0500

    adjust with AdministratorAbstract

--- src/Core/User/AdministratorBehavior.ts
+++ src/Core/User/AdministratorBehavior.ts
@@ -9,6 +9,7 @@ import { AppointmentData } from './../../Modules/AppointmentManagement/Appointme
 import { IDailyScheduleData, IWeeklyScheduleData, WeeklyScheduleData, DailyScheduleData } from './../../Modules/Schedule/ScheduleData';
 import { ScheduleBehavior } from './../../Modules/Schedule/ScheduleBehavior'
 import { SalonCloudResponse } from './../SalonCloudResponse'
+import { SalonTimeData } from './../../Core/SalonTime/SalonTimeData'
 
 export interface AdministratorBehavior {
     cancelAppointment(appointmentId: string);
@@ -16,8 +17,21 @@ export interface AdministratorBehavior {
     getAllEmployeeProfile(): Array<UserProfile>;
     getCustomerById(customerId: string): UserProfile;
     getEmployeeProfile(employeeId: string): UserProfile;
-    saveAppointment(appointment: AppointmentData);
+    saveAppointment(appointment: SaveAppointmentData);
     updateAppointment(appointment: AppointmentData);
-    updateDailySchedule(employeeId: string, dailySchedule: DailyScheduleData, schedule: ScheduleBehavior) : Promise<SalonCloudResponse<IDailyScheduleData>>;
-    updateWeeklySchedule(employeeId: string, weeklySchedule: WeeklyScheduleData, schedule: ScheduleBehavior) : Promise<SalonCloudResponse<IWeeklyScheduleData>>;
+    updateDailySchedule(employeeId: string, dailySchedule: DailyScheduleData, schedule: ScheduleBehavior): Promise<SalonCloudResponse<IDailyScheduleData>>;
+    updateWeeklySchedule(employeeId: string, weeklySchedule: WeeklyScheduleData, schedule: ScheduleBehavior): Promise<SalonCloudResponse<IWeeklyScheduleData>>;
+}
+export interface SaveAppointmentData {
+    note?: string,
+    customer_phone: string,
+    customer_name?: string,
+    salon_id: string,
+    services: Array<AppointmentService>
+}
+
+export interface AppointmentService {
+    service_id: string,
+    employee_id: string,
+    start: SalonTimeData
 }
