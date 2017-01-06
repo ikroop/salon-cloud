95bdfa348e5daeb53c2474fefbe004ac91500275
Thanh Truong <vagabondtt1503@gmail.com>
2017-01-06 00:00:24 -0500

    add createGroup method

--- src/Services/ServiceDatabase/Firebase/FirebaseServiceManagement.ts
+++ src/Services/ServiceDatabase/Firebase/FirebaseServiceManagement.ts
@@ -11,77 +11,67 @@ import { ServiceGroupData, ServiceItemData, IServiceGroupData, IServiceItemData 
 import { ServiceManagementDatabaseInterface } from './../ServiceManagementDatabaseInterface';
 import { firebase } from './../../Firebase';
 import { firebaseAdmin } from './../../FirebaseAdmin';
+import { FirebaseSalonManagement } from './../../SalonDatabase/Firebase/FirebaseSalonManagement';
 
-export class MongoServiceManagement implements ServiceManagementDatabaseInterface<IServiceGroupData, IServiceItemData> {
+export class FirebaseServiceManagement implements ServiceManagementDatabaseInterface<IServiceGroupData, IServiceItemData> {
     private salonId: string;
     private database: any;
     private serviceRef: any;
     private SERVICE_GROUP_KEY_NAME: string = 'service_groups';
-    private readonly SALON_KEY_NAME: string = 'salons';
     /**
-        * Creates an instance of MongoServiceManagement.
+        * Creates an instance of FirebaseServiceManagement.
         * 
         * @param {string} salonId
         * 
-        * @memberOf MongoServiceManagement
+        * @memberOf FirebaseServiceManagement
         */
     constructor(salonId: string) {
         this.salonId = salonId;
         this.database = firebaseAdmin.database();
-        this.serviceRef = this.database.ref(this.SERVICE_GROUP_KEY_NAME);
+        var salonDatabase = new FirebaseSalonManagement(salonId);
+        var salonRef = salonDatabase.getSalonFirebaseRef();
+        this.serviceRef = salonRef.ref(salonId + '/' + this.SERVICE_GROUP_KEY_NAME);
     }
 
+    /**
+     * 
+     * 
+     * @param {ServiceGroupData} group
+     * @returns {Promise<IServiceGroupData>}
+     * 
+     * @memberOf FirebaseServiceManagement
+     */
     public async createGroup(group: ServiceGroupData): Promise<IServiceGroupData> {
-        /*var rs: IServiceGroupData = undefined;
-    
-        var serviceGroupModel = new ServiceGroupModel(group);
-        var serviceCreation = serviceGroupModel.save();
-        await serviceCreation.then(function (docs) {
-            rs = docs;
-        }, function (err) {
-            throw err;
-        })
-        return rs;
-        */
-
-
+        var serviceGroup: IServiceGroupData = undefined;
+        var newGroup = await this.serviceRef.push();
+        await newGroup.save(group);
+        serviceGroup = await this.getServiceGroupById(newGroup.key);
+        return serviceGroup;
     }
 
     public async getAllServices(): Promise<IServiceGroupData[]> {
         var rs: IServiceGroupData[] = undefined;
-        await ServiceGroupModel.find({ salon_id: this.salonId }).exec(function (err, docs: IServiceGroupData[]) {
-            if (err) {
-                throw err;
-            } else {
-                rs = docs;
-            }
-        });
 
         return rs;
     }
 
     public async getServiceItemById(serviceId: string): Promise<IServiceItemData> {
-        var rs: IServiceItemData = undefined;
-        await ServiceGroupModel.findOne({ 'service_list': { '$elemMatch': { '_id': serviceId } } }).exec(function (err, docs: IServiceItemData) {
-            if (err) {
-                throw err;
-            } else {
-                rs = docs;
-            }
-        });
-
-        return rs;
+        return;
     }
 
     public async getServiceGroupByName(groupName: string): Promise<IServiceGroupData> {
-        var rs: IServiceGroupData = undefined;
-        await ServiceGroupModel.findOne({ 'name': groupName, 'salon_id': this.salonId }).exec(function (err, docs: IServiceGroupData) {
-            if (err) {
-                throw err;
-            } else {
-                rs = docs;
+        return;
+    }
+
+    private async getServiceGroupById(id: string): Promise<IServiceGroupData> {
+        var serviceGroup: IServiceGroupData = undefined;
+        await this.serviceRef.ref(id).once('value', async function (snapshot) {
+            serviceGroup = snapshot.val();
+            if (serviceGroup) {
+                serviceGroup._id = snapshot.key;
             }
         });
-        return rs;
+
+        return serviceGroup;
     }
 }
