80aabb8197acf26df603d0d76d4b9d4f03da65db
Due Nguyen <duenguyen213@gmail.com>
2017-01-10 12:27:21 -0500

    add shorter time service and longer time service

--- src/Core/DefaultData.ts
+++ src/Core/DefaultData.ts
@@ -68,13 +68,13 @@ const regularServiceItem1: ServiceItemData = {
 const regularServiceItem2: ServiceItemData = {
     name: 'Regular Manicure',
     price: 15,
-    time: 1800
+    time: 1200
 }
 
 const gelServiceItem1: ServiceItemData = {
     name: 'Gel Pedicure',
     price: 30,
-    time: 1800
+    time: 2400
 }
 
 const gelServiceItem2: ServiceItemData = {
