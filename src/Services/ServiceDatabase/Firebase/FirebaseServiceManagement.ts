53d2cc29475a2e12f418ce96018d51680e2cf4a0
Thanh Truong <vagabondtt1503@gmail.com>
2017-01-20 15:42:19 -0500

    add comment

--- src/Services/ServiceDatabase/Firebase/FirebaseServiceManagement.ts
+++ src/Services/ServiceDatabase/Firebase/FirebaseServiceManagement.ts
@@ -47,6 +47,8 @@ export class FirebaseServiceManagement implements ServiceManagementDatabaseInter
 
         //ready for creating service item id
         var serviceList = group.service_list;
+
+        //clear service List
         group.service_list = null;
 
         var newGroup = await this.serviceRef.push();
