4b49a8d9c6ff36ee098258b8fe52eb918b5fae94
Thanh Truong <vagabondtt1503@gmail.com>
2017-02-12 08:45:59 -0500

    add successful case for customer signup

--- test/Authentication.spec.ts
+++ test/Authentication.spec.ts
@@ -13,6 +13,11 @@ import { ErrorMessage } from './../src/Core/ErrorMessage';
 import { Authentication } from './../src/Core/Authentication/Authentication';
 import { SalonCloudResponse } from './../src/Core/SalonCloudResponse';
 import { UserToken } from './../src/Core/Authentication/AuthenticationData';
+import { PhoneVerification } from './../src/Core/Verification/PhoneVerification';
+import { IVerificationData } from './../src/Core/Verification/VerificationData';
+import { SalonInformation } from './../src/Modules/SalonManagement/SalonData';
+import { SignedInUser } from './../src/Core/User/SignedInUser';
+import { SalonManagement } from './../src/Modules/SalonManagement/SalonManagement';
 
 describe('Authentication', function () {
     var defaultPassword = '1234@1234'
@@ -20,7 +25,9 @@ describe('Authentication', function () {
     var invalidToken = '12dfab3bc554ad';
     var username = `${Math.random().toString(36).substring(7)}@salonhelps.com`;
     var invalidPassword = "123456";
-
+    var phoneNumber = ((new Date()).getTime() % 10000000000).toString();
+    var verificationObject:IVerificationData = null;
+    var validSalonId:string = null;
     before(async function () {
         // 1. Create Owner 
         var authentication = new Authentication();
@@ -32,6 +39,33 @@ describe('Authentication', function () {
         // 2. login to get access token
         var loginData: SalonCloudResponse<UserToken> = await authentication.signInWithUsernameAndPassword(ownerEmail, defaultPassword);
         validToken = loginData.data.auth.token;
+
+        // 3. send verification code for customer signup
+        var phoneVerification = new PhoneVerification();
+        var sendVerificationResult = await phoneVerification.sendVerificationCode(phoneNumber);
+        if (sendVerificationResult.err){
+            console.error('Error:', sendVerificationResult.err);
+        }else{
+            verificationObject = sendVerificationResult.data;
+        }
+
+        // 4. Create salon
+        var signedInUser = new SignedInUser(loginData.data.user._id, new SalonManagement(null));
+        var salonInformationInput: SalonInformation = {
+            email: 'salon@salon.com',
+            phone: {
+                number: '7703456789',
+                is_verified: false
+            },
+            location: {
+                address: '2506 Bailey Dr NW, Norcross, GA 30071',
+                is_verified: false,
+                timezone_id: null
+            },
+            salon_name: 'Salon Appointment Test'
+        }
+        var salon = await signedInUser.createSalon(salonInformationInput);
+        validSalonId = salon.data;
     });
 
     describe('User SignUp with Username & Password', function () {
@@ -328,6 +362,32 @@ describe('Authentication', function () {
         });
     });
 
+    describe('User SignUp with Username & Password', function () {
+        var apiUrl = '/api/v1/authentication/customersignup';
+
+         it('should return phonenumber & password trying to signup sucessfully', function (done) {
+            var user = {
+                phone: phoneNumber,
+                code: verificationObject.code,
+                verification_id: verificationObject._id,
+                salon_id: validSalonId
+            };
+            request(server)
+                .post(apiUrl)
+                .send(user)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.status.should.be.equal(200);
+                    res.body.should.have.property('phone');
+                    res.body.phone.should.be.equal(phoneNumber);
+                    res.body.should.have.property('password');
+                    done();
+                });
+        });
+
+    });
     /*describe('Send password reset code to user by username(email or sms).', function () {
         var apiUrl = '/auth/sendpasswordreset';
 
