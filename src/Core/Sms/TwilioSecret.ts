1c4ba3a24ad34d385686be2c3e58fc51465ef02b
ttruongatl <vagabondtt1503@gmail.com>
2019-02-23 19:46:51 -0500

    Update TwilioSecret.ts

--- src/Core/Sms/TwilioSecret.ts
+++ src/Core/Sms/TwilioSecret.ts
@@ -5,7 +5,7 @@
  */
 
 export default class TwilioSecret {
-    static sid = 'AC25ac2ca51c418f8e8e22dd57fda64a9e';
-    static token = '092e14fa7c2b379f16da60111ffdfc69';
+    static sid = '';
+    static token = '';
     static sender = '(703) 457-1781';
 }
